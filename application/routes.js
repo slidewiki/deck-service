0f641816c8cfb534be12a14684895a583f07502f
Roy Meissner <roy-meissner@gmx.net>
2016-02-19 17:57:12 +0100

    Partly implemented REST API

--- application/routes.js
+++ application/routes.js
@@ -1,13 +1,64 @@
-/* These are routes as defined in https://docs.google.com/document/d/1337m6i7Y0GPULKLsKpyHR4NRzRwhoxJnAZNnDFCigkc/edit#
-Each route implementes a basic parameter/payload validation and a swagger API documentation desription*/
-
 'use strict';
+
 const Joi = require('joi');
 const handlers = require('./controllers/handler');
 
 module.exports = function(server) {
+  server.route({
+    method: 'GET',
+    path: '/deck/{id}',
+    handler: null,
+    config: {
+      validate: {
+        params: {
+          id: Joi.number().integer().min(0)
+        },
+        response: Joi.object().keys({
+          id: Joi.number().integer().min(0),
+          title: Joi.string(),
+          description: Joi.string(),
+          numberOfSlides: Joi.integer().min(0),
+          created_at: Joi.date().timestamp()
+        }).requiredKeys('id', 'title', 'description', 'numberOfSlides', 'created_at')
+      },
+      tags: ['api'],
+      description: 'Get matadata of a deck'
+    }
+  });
+  /*TODO think about deck/slides - missing until now*/
+  /* Response:
+  {
+   title: 'root', id:56, type:'deck', position:’1’, children:
+      [
+        {title: 'child 1', id: 11, type: 'slide', position: ‘1’},
+        {title: 'child 2', id: 12, type: 'slide', , position: ‘2’},
+        {title: 'child 3', id: 13, type: 'deck', , position: ‘3’, children:[
+          {title: 'child 31', id: 131, type: 'slide' , position: ‘1’},
+          {title: 'child 32', id: 132, type: 'deck', position: ‘2’, children:[
+            {title: 'child 321', id: 1321, type: 'slide' , position: ‘1’},
+            {title: 'child 322', id: 1322, type: 'slide', position: ‘2’},
+        ]},
+        {title: 'child 33', id: 133, type: 'slide', position: ‘3’}
+        ]},
+        {title: 'child 4', id: 14, type: 'slide', position: ‘4’}
+      ]
+    }
+  */
+  server.route({
+    method: 'GET',
+    path: '/deck/tree/{id}',
+    handler: null,
+    config: {
+      validate: {
+        params: {
+          id: Joi.number().integer().min(0)
+        }
+      },
+      tags: ['api'],
+      description: 'Get the deck tree'
+    }
+  });
 
-  //Get slide with id id from database and return it (when not available, return NOT FOUND). Validate id to an integer, greater then 0
   server.route({
     method: 'GET',
     path: '/slide/{id}',
@@ -16,19 +67,23 @@ module.exports = function(server) {
       validate: {
         params: {
           id: Joi.number().integer().min(0)
-        }
+        },
+        response: Joi.object().keys({
+          id: Joi.number().integer().min(0),
+          title: Joi.string(),
+          body: Joi.string(),
+          created_at: Joi.date().timestamp()
+        }).requiredKeys('id', 'title', 'body', 'created_at')
       },
       tags: ['api'],
-      description: 'Get slide with id <id>'
+      description: 'Get a slide'
     }
   });
 
-  //Create new slide (by payload) and return it (...). Validate payload
   server.route({
     method: 'POST',
     path: '/slide/new',
     handler: handlers.newSlide,
-    //TODO validate body
     config: {
       validate: {
         payload: Joi.object().keys({
@@ -38,14 +93,22 @@ module.exports = function(server) {
           user_id: Joi.number().integer().min(0),
           root_deck_id: Joi.number().integer().min(0),
           language: Joi.string()
-        }).requiredKeys('title', 'language')
+        }).requiredKeys('title', 'language'),
+        response: Joi.object().keys({
+          id: Joi.number().integer().min(0),
+          title: Joi.string(),
+          body: Joi.string(),
+          created_at: Joi.date().timestamp(),
+          root_deck_changed: Joi.boolean(),
+          root_deck_id: Joi.number().integer().min(0)
+        }).requiredKeys('id', 'title', 'body', 'created_at', 'root_deck_changed', 'root_deck_id')
       },
       tags: ['api'],
       description: 'Create a new slide'
     }
   });
 
-  //Update slide with id id (by payload) and return it (...). Validate payload
+  // TODO Altered API from Alis proposal
   server.route({
     method: 'PUT',
     path: '/slide/{id}',
@@ -63,7 +126,15 @@ module.exports = function(server) {
           root_deck_id: Joi.number().integer().min(0),
           parent_deck_id: Joi.number().integer().min(0),
           no_new_revision: Joi.boolean()
-        }).requiredKeys('id', 'title', 'body')
+        }).requiredKeys('id', 'title', 'body'),
+        response: Joi.object().keys({
+          id: Joi.number().integer().min(0),
+          title: Joi.string(),
+          body: Joi.string(),
+          root_deck_changed: Joi.boolean(),
+          root_deck_id: Joi.number().integer().min(0),
+          created_at: Joi.date().timestamp()
+        }).requiredKeys('id', 'title', 'body', 'root_deck_changed', 'created_at')
       },
       tags: ['api'],
       description: 'Update a slide'
--- application/server.js
+++ application/server.js
@@ -1,16 +1,9 @@
-/*
-This is a demo application implementing some interfaces as described in https://docs.google.com/document/d/1337m6i7Y0GPULKLsKpyHR4NRzRwhoxJnAZNnDFCigkc/edit#
-This application demonstrates a serivce which returns previously inserted data from a fake database. The requests have no sideeffects and there is no database involved.
- */
-
 'use strict';
 
-//This is our webserver framework (instead of express)
 const hapi = require('hapi');
 const myRoutes = require('./routes.js');
 const co = require('./common');
 
-//Initiate the webserver
 const server = new hapi.Server();
 let port2 = 3000;
 if (!co.isEmpty(process.env.APPLICATION_PORT))
@@ -20,10 +13,8 @@ server.connection({
   port: port2
 });
 
-//Export the webserver to be able to use server.log()
 module.exports = server;
 
-//Plugin for sweet server console output
 let plugins = [
   require('inert'),
   require('vision'), {
@@ -37,7 +28,7 @@ let plugins = [
         }
       }]
     }
-  }, { //Plugin for swagger API documentation
+  }, {
     register: require('hapi-swagger'),
     options: {
       info: {
@@ -49,7 +40,6 @@ let plugins = [
   }
 ];
 
-//Register plugins and start webserver
 server.register(plugins, (err) => {
   if (err) {
     console.error(err);
@@ -57,7 +47,6 @@ server.register(plugins, (err) => {
   } else {
     server.start(() => {
       server.log('info', 'Server started at ' + server.info.uri);
-      //Register routes
       myRoutes(server);
     });
   }
