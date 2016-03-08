3c305cc304c73558fcc4485c676e1197c365d110
Roy Meissner <roy-meissner@gmx.net>
2016-03-08 10:29:53 +0100

    renamed docker repos

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t slidewiki/deckservice ./
-docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
+docker rmi $(docker images | grep "<none>" | awk "{print \$3}")
 docker push slidewiki/deckservice
