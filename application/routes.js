2beea06fbb06e73ad29848556d305a370d4ba3c0
Kurt Junghanns <tboonx@googlemail.com>
2017-02-22 17:42:33 +0100

    Fixed routes for update a deck #2

--- application/routes.js
+++ application/routes.js
@@ -250,7 +250,7 @@ module.exports = function(server) {
                             userid: Joi.number(),
                             username: Joi.string(),
                             joined: Joi.string(),
-                            picture: Joi.string()
+                            picture: Joi.string().allow('')
                         })).default([])
                     })
                 }).requiredKeys('user', 'license'),
