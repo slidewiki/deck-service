fd5b165be326bc09eb5d6d04dd67f2454fa8ecb2
Huw Fryer <huwfryer@gmail.com>
2017-05-17 19:50:23 +0100

    Removes unnecessary style information from template

--- application/controllers/handler.js
+++ application/controllers/handler.js
@@ -22,12 +22,12 @@ const userService = require('../services/user');
 const tagService = require('../services/tag');
 
 const slidetemplate = '<div class="pptx2html" style="position: relative; width: 960px; height: 720px;">'+
-    '<p></p><p></p><p></p><p></p><p></p><div _id="2" _idx="undefined" _name="Title 1" _type="title" class="block content v-mid" style="position: absolute; top: 38.3334px; left: 66px; width: 828px; height: 139.167px; z-index: 23488;">'+
-    '<h3 class="h-mid"><span class="text-block" style="font-weight: initial; font-style: normal; text-decoration: initial; vertical-align: ;">Title</span></h3></div>'+
+    '<div _id="2" _idx="undefined" _name="Title 1" _type="title" class="block content v-mid h-mid" style="position: absolute; top: 38.3334px; left: 66px; width: 828px; height: 139.167px; z-index: 23488;">'+
+    '<h3 class="h-mid">Title</h3></div>'+
     '<div _id="3" _idx="1" _name="Content Placeholder 2" _type="body" class="block content v-up" style="position: absolute; top: 191.667px; left: 66px; width: 828px; height: 456.833px; z-index: 23520;">'+
     '<ul>'+
-    '	<li class="h-left" style="text-align: left;"><span class="text-block" style="font-weight: initial; font-style: normal; text-decoration: initial; vertical-align: ;">Text bullet 1</span></li>'+
-    '	<li class="h-left" style="text-align: left;"><span class="text-block" style="font-weight: initial; font-style: normal; text-decoration: initial; vertical-align: ;">Text bullet 2</span></li>'+
+    '	<li>Text bullet 1</li>'+
+    '	<li>Text bullet 2</li>'+
     '</ul>'+
     '<div class="h-left">&nbsp;</div>'+
     '</div></div>';
