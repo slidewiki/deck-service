012b55d1283d78160fc37a963b293a7ad8f45dad
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2017-06-21 13:06:00 +0300

    minor fix in user microservices uri

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -7,7 +7,7 @@ module.exports = {
         uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'http://fileservice',
     },
     'user': {
-        uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'https://userservice.experimental.slidewiki.org',
+        uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'http://userservice',
     },
     'tag': {
         uri: (!co.isEmpty(process.env.SERVICE_URL_TAG)) ? process.env.SERVICE_URL_TAG : 'http://tagservice',
