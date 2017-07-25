ecc2762fc4f3b4f4d1f41ea5f9d6d8b667bac167
DaryaTarasowa <darya.tarasowa@gmail.com>
2017-07-25 13:39:57 +0200

    Create microservices.sample.js

---
+++ application/configs/microservices.sample.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const co = require('../common');
+
+module.exports = {
+    'file': {
+        uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'http://fileservice',
+    },
+    'user': {
+        uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'http://userservice',
+    },
+    'tag': {
+        uri: (!co.isEmpty(process.env.SERVICE_URL_TAG)) ? process.env.SERVICE_URL_TAG : 'http://tagservice',
+    }
+};
