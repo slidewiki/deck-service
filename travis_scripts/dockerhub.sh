ca7c7bd9bfd9d6528e11d4e583580f748ea8cbc3
Kostis Pristouris <kprist@gmail.com>
2018-02-08 16:07:22 +0200

    Fix travis docker push script error

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build--build-arg BUILD_ENV=travis -t slidewiki/deckservice:latest-dev ./
+docker build --build-arg BUILD_ENV=travis -t slidewiki/deckservice:latest-dev .
 docker push slidewiki/deckservice:latest-dev
