ea3dca04dd060c292b7a4e2dbc36d8ad0f6ae35c
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2018-08-09 15:45:39 +0300

    Add indexes for groups collection

--- application/database/createIndexes.js
+++ application/database/createIndexes.js
@@ -3,7 +3,7 @@
 const helper = require('./helper');
 
 module.exports = function() {
-    return helper.getCollection('decks').then((decks) => {
+    let decksIndexes = helper.getCollection('decks').then((decks) => {
         return decks.createIndexes([
             { key: {'revisions.contentItems.ref.id': 1} },
             { key: {'revisions.contentItems.ref.revision': 1} },
@@ -11,4 +11,14 @@ module.exports = function() {
             { key: {'origin.id': 1} },
         ]);
     });
+
+    let groupsIndexes = helper.getCollection('groups').then((groups) => {
+    	return groups.createIndexes([
+            { key: {'user': 1} },
+            { key: {'userGroup': 1} },
+            { key: {'decks': 1} },
+        ]);
+    });
+
+    return Promise.all([decksIndexes, groupsIndexes]);
 };
